8137ab908c8ca25b949084bb97e56b039fbfabfe
Aux <justin.gentry@auxesis.tv>
2017-12-23 21:03:11 -0600

    Messing around with inmemory

--- src/Databases/RootDatabase.cs
+++ src/Databases/RootDatabase.cs
@@ -16,13 +16,13 @@ namespace Colliebot
         public DbSet<DbDiscordGuildUser> DiscordGuildUsers { get; private set; }
         public DbSet<DbDiscordUser> DiscordUsers { get; private set; }
 
-        public RootDatabase()  // Migrations Constructor
-        {
-            _config = new ConfigurationBuilder()
-                .SetBasePath(AppContext.BaseDirectory)
-                .AddJsonFile("_configuration.json")
-                .Build();
-        }
+        //public RootDatabase()  // Migrations Constructor
+        //{
+        //    _config = new ConfigurationBuilder()
+        //        .SetBasePath(AppContext.BaseDirectory)
+        //        .AddJsonFile("_configuration.json", optional: true)
+        //        .Build();
+        //}
 
         public RootDatabase(IConfigurationRoot config)
         {
@@ -31,18 +31,16 @@ namespace Colliebot
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            string connection = $"Server={_config["postgre:server"]};" +
-                $"Port={_config["postgre:port"]};" +
-                $"Guild ID={_config["postgre:Guild"]};" +
-                $"Password={_config["postgre:password"]};" +
-#if RELEASE
-                $"Database=root;";
-#elif DEBUG
-                $"Database=test;";
-#endif
-            builder.UseNpgsql(connection);
-        }
+            builder.UseInMemoryDatabase("testing");
 
+            //string connection = $"Server={_config["postgre:server"]};" +
+            //    $"Port={_config["postgre:port"]};" +
+            //    $"User ID={_config["postgre:user"]};" +
+            //    $"Password={_config["postgre:password"]};" +
+            //    $"Database=test;";
+            //builder.UseNpgsql(connection);
+        }
+        
         protected override void OnModelCreating(ModelBuilder builder)
         {
             DbDiscordGuild.AddModel(builder);
